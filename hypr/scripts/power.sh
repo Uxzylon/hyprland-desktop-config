822e8ef1578a84e34f669d2113472a3fbab635c4
anthony.jeanney <anthony.jeanney@gmail.com>
2024-01-10 10:07:09 +0100

    Fix Power menu not closing on second click

--- hypr/scripts/power.sh
+++ hypr/scripts/power.sh
@@ -5,6 +5,11 @@ CONFIG="$HOME/.config/wofi/WofiBig/config"
 STYLE="$HOME/.config/wofi/style.css"
 COLORS="$HOME/.config/wofi/colors"
 
+if [[ $(pidof wofi) ]]; then
+	pkill wofi
+	exit
+fi
+
 # Wofi window config (in %)
 WOFI_WIDTH=5
 WOFI_HEIGHT=23
@@ -16,18 +21,20 @@ wofi_command="wofi --show dmenu \
 			--cache-file=/dev/null \
 			--hide-scroll --no-actions \
 			--matching=fuzzy"
-			
+
 entries=$(echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | $wofi_command -i --dmenu | awk '{print tolower($2)}')
 
-case $entries in 
-    poweroff|reboot|suspend)
-        systemctl $entries
-        ;;
-    lock)
-        sleep 0.5s; swaylock & disown
-        ;;
-    logout)
-        hyprctl dispatch exit 0
-        ;;
+case $entries in
+poweroff | reboot | suspend)
+	systemctl $entries
+	;;
+lock)
+	sleep 0.5s
+	swaylock &
+	disown
+	;;
+logout)
+	hyprctl dispatch exit 0
+	;;
 esac
- 
+
