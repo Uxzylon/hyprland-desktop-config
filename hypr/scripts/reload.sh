ea23ff14cf9a8fa3cb457611611e5e17114ece37
anthony.jeanney <anthony.jeanney@gmail.com>
2024-01-13 10:41:39 +0100

    Wallpaper reload

--- hypr/scripts/reload.sh
+++ hypr/scripts/reload.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-_ps=(waybar mako dunst wofi)
+_ps=(waybar mako dunst wofi swww)
 for _prs in "${_ps[@]}"; do
-    if [[ $(pidof ${_prs}) ]]; then
-        killall -9 ${_prs}
-    fi
+	if [[ $(pidof ${_prs}) ]]; then
+		killall -9 ${_prs}
+	fi
 done
 
 hyperctl reload
 dunst -conf $HOME/.config/dunst/dunstrc &
 waybar --bar main-bar --log-level error --config $HOME/.config/waybar/config --style $HOME/.config/waybar/style.css &
+swww init &
+swww img $SWWW_WALLPAPER &
