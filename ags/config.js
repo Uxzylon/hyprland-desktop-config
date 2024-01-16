789bfc1be20816ddcb07978bd47655470bbb286d
anthony.jeanney <anthony.jeanney@gmail.com>
2024-01-17 00:06:55 +0100

    Switched to SCSS pre-processor

--- ags/config.js
+++ ags/config.js
@@ -1,18 +1,14 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import Gdk from 'gi://Gdk';
-import { monitorFile } from 'resource:///com/github/Aylur/ags/utils.js';
+import { exec } from 'resource:///com/github/Aylur/ags/utils.js';
 import Bar from './bar/Bar.js';
 import AppLauncher from './applauncher/AppLauncher.js';
 import PowerMenu from './powermenu/PowerMenu.js';
 import { notificationPopup } from './notification-popups/NotificationPopups.js';
 
-monitorFile(
-    `${App.configDir}/style.css`,
-    function() {
-        App.resetCss();
-        App.applyCss(`${App.configDir}/style.css`);
-    },
-);
+const scss = `${App.configDir}/style.scss`
+const css = `${App.configDir}/style.css`
+exec(`sassc ${scss} ${css}`)
 
 export function range(length, start = 1) {
     return Array.from({ length }, (_, i) => i + start);
@@ -23,15 +19,13 @@ export function forMonitors(widget) {
     return range(n, 0).map(widget);
 }
 
-const windows = () => [
-    forMonitors(Bar),
-    AppLauncher(),
-    PowerMenu(),
-    notificationPopup,
-];
-
 // exporting the config so ags can manage the windows
 export default {
-    style: App.configDir + '/style.css',
-    windows: windows(),
+    style: css,
+    windows: [
+        forMonitors(Bar),
+        AppLauncher(),
+        PowerMenu(),
+        notificationPopup,
+    ],
 };
